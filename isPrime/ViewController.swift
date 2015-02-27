850dab5c04ca3d03b2f1d708ae8909e738d14f4c
HoodsDream <idinkyy@gmail.com>
2015-02-27 14:55:43 -0800

    Swift tool for checking if a number is Prime

--- isPrime/ViewController.swift
+++ isPrime/ViewController.swift
@@ -10,10 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var number: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        var numberInt = number.text.toInt()
+        
+        if numberInt != nil {
+            
+            var unwrappedNumber = numberInt!
+            
+            var isPrime = true
+            
+            if unwrappedNumber == 1 {
+                isPrime = false
+            }
+            
+            if unwrappedNumber != 1 && unwrappedNumber != 2 {
+                for var i = 2; i < unwrappedNumber; i++ {
+                    if unwrappedNumber % i == 0 {
+                        isPrime = false
+                    }
+                }
+            }
+            
+            if isPrime == true {
+                resultLabel.text = "\(unwrappedNumber) is a prime number!"
+            } else {
+                resultLabel.text = "\(unwrappedNumber) is not a prime number!"
+            }
+            
+        } else {
+            resultLabel.text = "Please enter a number"
+            
+        }
+        
     }
+    
+    
+       override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
